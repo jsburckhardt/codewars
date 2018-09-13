37425841a71e44368ecea3bdc9b0dc3b91563548
juan <jsburckhardt@gmail.com>
2018-09-13 23:15:48 +1000

    plusminus hacker rank

---
+++ plusminus/plusminus.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the plusMinus function below.
+func plusMinus(arr []int32) {
+	var pos, neg, zero int
+	for _, v := range arr {
+		switch {
+		case v > 0:
+			pos++
+		case v < 0:
+			neg++
+		case v == 0:
+			zero++
+		}
+	}
+	fmt.Println(fmt.Sprintf("%0.6f", float64(pos)/float64(len(arr))))
+	fmt.Println(fmt.Sprintf("%0.6f", float64(neg)/float64(len(arr))))
+	fmt.Println(fmt.Sprintf("%0.6f", float64(zero)/float64(len(arr))))
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(readLine(reader), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	plusMinus(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
